f5192c50c6b14da8d9646a9460a966254b7df29c
Alex Morega <alex@grep.ro>
2013-02-27 16:40:52 +0200

    show properties of an object

--- browser.py
+++ browser.py
@@ -1,5 +1,8 @@
+import os
 import flask
+import sparql
 
+ENDPOINT = os.environ['SPARQL_ENDPOINT']
 
 browser = flask.Blueprint('browser', __name__)
 
@@ -7,3 +10,13 @@ browser = flask.Blueprint('browser', __name__)
 @browser.route('/')
 def home():
     return 'hi'
+
+
+@browser.route('/objinfo')
+def objinfo():
+    subject = sparql.IRI(flask.request.args['s'])
+    query = flask.render_template('objinfo.sparql', **{'subject': subject})
+    return flask.render_template('objinfo.html', **{
+        'subject': subject,
+        'rows': sparql.query(ENDPOINT, query),
+    })
