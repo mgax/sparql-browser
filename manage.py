27bc585418213311aacdd6a7a5efab82193e6207
Alex Morega <alex@grep.ro>
2013-02-27 16:37:20 +0200

    DEBUG flag

--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
+import os
 import flask
 from flask.ext.script import Manager
+
+DEBUG = (os.environ.get('DEBUG') == 'on')
 
 
 def create_app():
     import browser
     app = flask.Flask(__name__)
+    app.debug = DEBUG
     app.register_blueprint(browser.browser)
     return app
 
