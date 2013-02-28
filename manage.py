4c41711c54639d21c1c7ba4a56ce1b3ee8c7b4b4
Alex Morega <alex@grep.ro>
2013-02-28 16:48:11 +0200

    allow reverse proxy

--- manage.py
+++ manage.py
@@ -7,11 +7,26 @@ from flask.ext.script import Manager
 DEBUG = (os.environ.get('DEBUG') == 'on')
 
 
+def proxy_fix(app, varname='HTTP_X_FORWARDED_SCRIPT_NAME'):
+    from werkzeug.contrib.fixers import ProxyFix
+    hardcoded_script_name = os.environ.get('HARDCODED_SCRIPT_NAME')
+    wsgi = app.wsgi_app
+    def middleware(environ, start_response):
+        if varname in environ:
+            environ['SCRIPT_NAME'] = environ[varname]
+        elif hardcoded_script_name:
+            environ['SCRIPT_NAME'] = hardcoded_script_name
+        return wsgi(environ, start_response)
+    app.wsgi_app = ProxyFix(middleware)
+
+
 def create_app():
     import browser
     app = flask.Flask(__name__)
     app.debug = DEBUG
     app.register_blueprint(browser.browser)
+    if os.environ.get('REVERSE_PROXY') == 'on':
+        proxy_fix(app)
     return app
 
 
